a8211f050ed153c8f1b36f6ceba69ae19186d32e
aroraayush14 <aroraayush14@gmail.com>
2023-08-31 20:01:45 +0530

    Working Code

--- crypto_proj/crypto_proj/urls.py
+++ crypto_proj/crypto_proj/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from positions.view import home_view
+from positions.views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(', home_view', name = 'home')
+    path('',home_view, name = 'home')
 ]
--- crypto_proj/positions/views.py
+++ crypto_proj/positions/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from requests import request
+import requests
+from django.http import HttpResponse
 # Create your views here.
 
-def home_view(request):
+def home_view(requests):
     url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-    data = request.get(url).json()
+    data = requests.get(url).json()
+
+    
+    context = {'data':data}
+    return render(requests, 'postions/main.html',context)
